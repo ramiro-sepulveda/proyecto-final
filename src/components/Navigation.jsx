de043b3598c8d6472b03feb3438997c5e57ccc75
nachytto <nacho.duartemiranda@gmail.com>
2024-11-09 19:22:10 -0300

    export navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
+const navigation = () => (
   <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
       <Navbar.Brand as={Link} to="/proyecto-final/">MarketPlace</Navbar.Brand>
@@ -17,4 +17,4 @@ const Header = () => (
   </Navbar>
 );
 
-export default Header;
+export default navigation;
