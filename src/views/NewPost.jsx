d70332dea1d4c96d7458a5613ba63a8e281fd89f
ramirosepulveda <ramirooooh14@gmail.com>
2024-11-10 13:24:05 -0300

    borrador de view publicaciones

--- src/views/NewPost.jsx
+++ src/views/NewPost.jsx
@@ -7,29 +7,43 @@ const NewPost = () => {
 
   return (
 
-    <Form>
+    <div className='w-100 align-self-center'>
+      <h2 className='pb-4 text-center fs-46px'>Nueva Publicación</h2>
+      <Form>
 
-      <Row className="min-vh-100 d-flex align-items-center">
-        <Col md={6} className="p-5 d-flex">
-        </Col>
-        <Col md={6} className="p-5 d-flex">
-        </Col>
-      </Row>
+        <Row className="d-flex align-items-center">
+          <Col md={6} className="px-5">
+            <Form.Group controlId="formBasicName" className="d-flex flex-column align-items-center gap-5">
+              <Form.Control type="tittle" placeholder="Título" className='custom-input w-75 rounded-4' />
+              <Form.Control type="price" placeholder="Precio" className='custom-input  w-75 rounded-4' />
+            </Form.Group>
+          </Col>
 
-      <Row className="min-vh-100 d-flex align-items-center">
-        <Col md={6} className="p-5 d-flex">
-        </Col>
-        <Col md={6} className="p-5 d-flex">
-        </Col>
-      </Row>
+          <Col md={6} className="px-5">
+            <Form.Group controlId="formBasicName" className="d-flex flex-column align-items-center gap-5">
+              <Form.Control type="label1" placeholder="Categoria 1" className='custom-input w-75 rounded-4' />
+              <Form.Control type="label2" placeholder="Categoria 2" className='custom-input  w-75 rounded-4' />
+            </Form.Group>
+          </Col>
 
-      <Row className="min-vh-100 d-flex align-items-center">
-        <Col md={6} className="p-5 d-flex">
-        </Col>
-        <Col md={6} className="p-5 d-flex">
-        </Col>
-      </Row>
+        </Row>
+        <Row className="d-flex align-items-center mt-5 mb-5">
+          <Col md={6} className="px-5">
+            <Form.Group controlId="formBasicName" className="d-flex flex-column align-items-center gap-5">
+              <Form.Control as="textarea" type="text" placeholder="Descripción" className='custom-input w-75 rounded-4'style={{ height: '160px' }}  />
+            </Form.Group>
+          </Col>
+
+          <Col md={6} className="px-5">
+            <Form.Group controlId="formBasicName" className="d-flex flex-column align-items-center gap-5">
+              <Form.Control type="name" placeholder="IMAGENES" className='custom-input w-75 rounded-4' />
+
+            </Form.Group>
+          </Col>
+
+        </Row>
       </Form>
+    </div>
   )
 };
 
