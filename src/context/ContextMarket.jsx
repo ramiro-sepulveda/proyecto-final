f69db47f6ccbae96d46b60be5112239f0b781dde
Ramiro Sepulveda <rgsepulveda@uc.cl>
2024-11-24 22:41:19 -0300

    arreglos deploy

--- src/context/ContextMarket.jsx
+++ src/context/ContextMarket.jsx
@@ -1,46 +1,46 @@
 import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { apiFavoritos } from '../api/apiFavoritos'; // Ajusta la ruta según la ubicación de tu archivo apiFavoritos.jsx
+import { apiFavoritos } from "../api/apiFavoritos"; // Ajusta la ruta según la ubicación de tu archivo apiFavoritos.jsx
 
-const MarketContext = createContext();
+export const MarketContext = createContext();
 
 const MarketProvider = ({ children }) => {
   const navigate = useNavigate();
   const [productos, setProductos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [carrito, setCarrito] = useState([]);
-  const [favoritos, setFavoritos] = useState([]);// Nuevo estado para favoritos
+  const [favoritos, setFavoritos] = useState([]); // Nuevo estado para favoritos
   const [usuario, setUsuario] = useState(false); // Usuario completo
   const [registro, setRegistro] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
   const login = (newToken) => {
-    localStorage.setItem('token', 'Bearer ' + newToken);
-    setToken(localStorage.getItem('token'));
+    localStorage.setItem("token", "Bearer " + newToken);
+    setToken(localStorage.getItem("token"));
     setIsAuthenticated(true);
   };
 
   const logout = () => {
     setToken(null);
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setIsAuthenticated(false);
-    navigate('/');
+    navigate("/");
   };
 
   useEffect(() => {
     const fetchFavoritos = async () => {
       try {
-        if (usuario?.id) {  // Verifica si el usuario tiene un ID válido
+        if (usuario?.id) {
+          // Verifica si el usuario tiene un ID válido
           const favoritos2 = await apiFavoritos.obtenerFavoritos(usuario.id);
           if (favoritos2) {
             setFavoritos(favoritos2);
-          } 
-          else {
-            console.log("hola");// Pasa el usuarioId correcto
-          
-        };
-      }} catch (error) {
+          } else {
+            console.log("hola"); // Pasa el usuarioId correcto
+          }
+        }
+      } catch (error) {
         console.error("Error al obtener los favoritos:", error);
       }
     };
@@ -53,11 +53,28 @@ const MarketProvider = ({ children }) => {
   }, [favoritos]);
 
   return (
-    <MarketContext.Provider value={{
-      setProductos, token, setToken, setIsAuthenticated, isAuthenticated, login, logout,
-      registro, setRegistro, productos, loading, carrito, setCarrito, favoritos, setFavoritos, 
-      usuario, setUsuario, setLoading
-    }}>
+    <MarketContext.Provider
+      value={{
+        setProductos,
+        token,
+        setToken,
+        setIsAuthenticated,
+        isAuthenticated,
+        login,
+        logout,
+        registro,
+        setRegistro,
+        productos,
+        loading,
+        carrito,
+        setCarrito,
+        favoritos,
+        setFavoritos,
+        usuario,
+        setUsuario,
+        setLoading,
+      }}
+    >
       {children}
     </MarketContext.Provider>
   );
